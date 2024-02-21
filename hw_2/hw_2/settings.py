0d086ff8132388942039eb08214fd66b1f5cfc28
angilbrt <uv04@bk.ru>
2024-02-21 15:17:46 +0600

    tutut

--- hw_2/hw_2/settings.py
+++ hw_2/hw_2/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,11 +56,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'hw_2.urls'
 
+print(os.path.join(BASE_DIR, 'movie', 'templates'))
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates'
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
--- hw_2/movie/views.py
+++ hw_2/movie/views.py
@@ -4,4 +4,4 @@ from .models import Product
 
 def get_ingex_page(request):
     products = Product.objects.all()
-    return render(request, 'index.html', {'products': products})
+    return render(request, 'products-page.html', {'products': products})
